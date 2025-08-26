f1d853c2dda2b48e0928d027e29eb428101fddfe
SAHIBZADA-DANY <angelgaming331@gmail.com>
2025-08-26 21:10:56 +0500

    Create numbers.java

---
+++ numbers.java
@@ -0,0 +1,10 @@
+public class numbers {
+    public static void main(String[] args){
+        System.out.println(3000);
+        System.out.println("My name is danial Ahmed");
+        System.out.println(3+2);
+        System.out.println("10"+5);
+        System.out.println(32.56e5f);
+        System.out.println(2e3);
+    }
+}
