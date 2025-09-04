cedcd2215f203919dbe20898fe387f7cdf5869bc
SAHIBZADA-DANY <angelgaming331@gmail.com>
2025-09-04 22:28:47 +0500

    Update numbers.java

--- numbers.java
+++ numbers.java
@@ -1,10 +1,11 @@
 public class numbers {
     public static void main(String[] args){
-        System.out.println(3000);
-        System.out.println("My name is danial Ahmed");
-        System.out.println(3+2);
-        System.out.println("10"+5);
-        System.out.println(32.56e5f);
-        System.out.println(2e3);
+        // System.out.println(3000);
+        // System.out.println("My name is danial Ahmed");
+        int x=2,y=3;
+        System.out.println("The sum of "+x+" and "+y+" is :"+(x+y));
+        // System.out.println("10"+5);
+        // System.out.println(32.56e5f);
+        // System.out.println(2e3);
     }
 }
