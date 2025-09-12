80fd2177be524e4f6acdc9757b398463f625b7b7
SAHIBZADA-DANY <angelgaming331@gmail.com>
2025-09-12 11:11:51 +0500

    Update TikTacToe.java

--- TikTacToe.java
+++ TikTacToe.java
@@ -13,6 +13,8 @@ public class TikTacToe {
     static void main(String[] args){
         char a='1',b='2',c='3',d='4',e='5',f='6',g='7',h='8',i='9';
         table(a,b,c,d,e,f,g,h,i);
+        char ch;
+        int move = 0;
         
     }
 }
