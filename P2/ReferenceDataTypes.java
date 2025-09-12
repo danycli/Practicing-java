28d711f7c78b1da0256568fe24cd783f55809029
SAHIBZADA-DANY <angelgaming331@gmail.com>
2025-09-12 12:20:11 +0500

    Create ReferenceDataTypes.java

---
+++ P2/ReferenceDataTypes.java
@@ -0,0 +1,10 @@
+package P2;
+
+import java.util.Date;
+
+public class ReferenceDataTypes{
+    public static void main(String[] args){
+        Date atTheMoment = new Date();
+        System.out.println(atTheMoment);
+    }
+}
