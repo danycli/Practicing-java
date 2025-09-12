67595ccf1a11843d4adfcb74d02932f52d2e5917
SAHIBZADA-DANY <angelgaming331@gmail.com>
2025-09-12 12:19:53 +0500

    Create dataTypes.java

---
+++ P1/dataTypes.java
@@ -0,0 +1,11 @@
+package P1;
+public class dataTypes{
+    public static void main(String[] args){
+        int viewCount = 223_312_321;
+        long hbNo = 998-331-423L;
+        byte wbcNo = 127;
+        char ch = 'A';
+        float num = 10.5F;
+        System.out.println(viewCount);
+    }
+}
