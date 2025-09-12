eab80a068bb6e32991da2e0d24b0ad1f43d7b132
SAHIBZADA-DANY <angelgaming331@gmail.com>
2025-09-12 17:47:25 +0500

    Create Strings.java

---
+++ P4/Strings.java
@@ -0,0 +1,8 @@
+package P4;
+public class Strings{
+    public static void main(String[] args){
+        String s = "What is your name...?";
+        System.out.println(s.indexOf('a'));
+        System.out.println(s.length());
+    }
+}
