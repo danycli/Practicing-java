50454fa3fee3cced24feaead44d98fe9bed311f0
SAHIBZADA-DANY <angelgaming331@gmail.com>
2025-09-14 23:15:08 +0500

    Create Maths.java

---
+++ P8/Maths.java
@@ -0,0 +1,11 @@
+package P8;
+public class Maths{
+    public static void main(String[] args){
+        int y = Math.round(4.5f);
+        System.out.println(y);
+        int r = (int)Math.ceil(4.3f);
+        System.out.println(r);
+        int g = (int) Math.floor(9.3f);
+        System.out.println(g);
+    }
+}
