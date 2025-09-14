ce03b4bfe333f8df102a1ae760aab9ec25b3d723
SAHIBZADA-DANY <angelgaming331@gmail.com>
2025-09-14 23:15:35 +0500

    Create bool.java

---
+++ practice/bool.java
@@ -0,0 +1,8 @@
+public class bool {
+    public static void main(String[] args) {
+        boolean name = true;
+        boolean isRamPresesnt = false;
+        System.out.println(name);
+        System.out.println(isRamPresesnt);
+    }
+}
