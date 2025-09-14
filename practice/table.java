0bd0ac8ff82867d61b2001911e2c00c97ebc3aed
SAHIBZADA-DANY <angelgaming331@gmail.com>
2025-09-14 23:17:41 +0500

    Create table.java

---
+++ practice/table.java
@@ -0,0 +1,15 @@
+public class table {
+    public void main(String[] args){
+        char a='1',b='2',c='3',d='4',e='5',f='6',g='7',h='8',i='9';
+        System.out.println("     |     |");
+        System.out.println("   "+a+" |  "+b+"  |  "+c+"");
+        System.out.println("_____|_____|_____");
+        System.out.println("     |     |");
+        System.out.println("   "+d+" |  "+e+"  |  "+f+"");
+        System.out.println("_____|_____|_____");
+        System.out.println("     |     |");
+        System.out.println("   "+g+" |  "+h+"  |  "+i+"");
+        System.out.println("     |     |");
+        
+    }
+}
