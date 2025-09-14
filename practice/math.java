bf4ef9aad71c16cb87f4ec87f51c0ca83cecd7b2
SAHIBZADA-DANY <angelgaming331@gmail.com>
2025-09-14 23:16:02 +0500

    Create math.java

---
+++ practice/math.java
@@ -0,0 +1,13 @@
+public class math {
+    public static void main (String[] args){
+        int x = 10;
+        int y = -55;
+        System.out.println(Math.max(x,y));
+        System.out.println(Math.min(x, y));
+        System.out.println(Math.abs(y));
+        System.out.println(Math.sqrt(x));
+        System.out.println(Math.random());
+        System.out.println(Math.pow(y,x));
+        System.out.println((int)(Math.random()*1001));
+    }
+}
