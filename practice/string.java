4c83c8cdd2db2e943eecf4dec9849f1fc1c5adbe
SAHIBZADA-DANY <angelgaming331@gmail.com>
2025-09-14 23:17:23 +0500

    Create string.java

---
+++ practice/string.java
@@ -0,0 +1,10 @@
+public class string {
+    public static void main(String[] args) {
+        String name = "I am the man of Fortune, and I must seek my Fortune";
+        System.out.println("Length = " + name.length());
+        System.out.println("Lower Case = " + name.toLowerCase());
+        System.out.println("Upper Case = " + name.toUpperCase());
+        System.out.println(name.indexOf('A'));
+        System.out.println(name.indexOf("Danial"));
+    }
+}
