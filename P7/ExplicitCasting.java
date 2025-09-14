6967bda2929670f2b03b00301c85d69e10850945
SAHIBZADA-DANY <angelgaming331@gmail.com>
2025-09-14 23:14:58 +0500

    Create ExplicitCasting.java

---
+++ P7/ExplicitCasting.java
@@ -0,0 +1,10 @@
+package P7;
+
+public class ExplicitCasting {
+    public static void main(String[] args) {
+        String a = "34";
+        int y = 9;
+        int sum = Integer.parseInt(a) + y;
+        System.out.println(sum);
+    }
+}
