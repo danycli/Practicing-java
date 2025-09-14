0eb2efdb173e10b2f5984181d292a86b3c3d6c71
SAHIBZADA-DANY <angelgaming331@gmail.com>
2025-09-14 23:13:30 +0500

    Delete bool.java

--- bool.java
+++
@@ -1,8 +0,0 @@
-public class bool {
-    public static void main(String[] args) {
-        boolean name = true;
-        boolean isRamPresesnt = false;
-        System.out.println(name);
-        System.out.println(isRamPresesnt);
-    }
-}
