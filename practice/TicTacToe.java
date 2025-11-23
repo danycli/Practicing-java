78b4d3dd500d2f39000a634685e9653e493e26ec
SAHIBZADA-DANY <angelgaming331@gmail.com>
2025-11-23 14:05:00 +0500

    bug making

--- practice/TicTacToe.java
+++ practice/TicTacToe.java
@@ -1,18 +1,6 @@
+package practice;
 import java.util.Scanner;
-
-public class TikTacToe {
-    static void table(char a, char b, char c, char d, char e, char f, char g, char h, char i) {
-        System.out.println("     |     |");
-        System.out.println("   " + a + " |  " + b + "  |  " + c + "");
-        System.out.println("_____|_____|_____");
-        System.out.println("     |     |");
-        System.out.println("   " + d + " |  " + e + "  |  " + f + "");
-        System.out.println("_____|_____|_____");
-        System.out.println("     |     |");
-        System.out.println("   " + g + " |  " + h + "  |  " + i + "");
-        System.out.println("     |     |");
-    }
-
+public class TicTacToe{
     public static void main(String[] args) {
         char a = '1', b = '2', c = '3', d = '4', e = '5', f = '6', g = '7', h = '8', i = '9';
         table(a, b, c, d, e, f, g, h, i);
@@ -147,4 +135,5 @@ public class TikTacToe {
         }
         pl.close();
     }
+}
 }
---
+++ practice/tableee.java
@@ -0,0 +1,14 @@
+package practice;
+public class tableee {
+    static void table(char a, char b, char c, char d, char e, char f, char g, char h, char i) {
+        System.out.println("     |     |");
+        System.out.println("   " + a + " |  " + b + "  |  " + c + "");
+        System.out.println("_____|_____|_____");
+        System.out.println("     |     |");
+        System.out.println("   " + d + " |  " + e + "  |  " + f + "");
+        System.out.println("_____|_____|_____");
+        System.out.println("     |     |");
+        System.out.println("   " + g + " |  " + h + "  |  " + i + "");
+        System.out.println("     |     |");
+    }
+}
